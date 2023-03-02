2376e36ba577d771b1677ee036d7d5deae7bcc30
Anthony Destenay <anthony.destenay@gmail.com>
2023-03-02 19:05:44 +0100

    Fixed meta in blog

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -2,6 +2,16 @@
   <Head>
     <Title>{{ data?.postBy?.title }}</Title>
     <Meta name="description" :content="data?.postBy?.excerpt" />
+    <Meta property="og:type" content="website" />
+    <Meta property="og:url" :content="loc.origin + loc.pathname" />
+    <Meta property="og:title" :content="data?.postBy?.title" />
+    <Meta property="og:description" :content="data?.postBy?.excerpt" />
+    <Meta property="og:image" :content="data?.postBy?.featuredImage.node.mediaItemUrl" />
+    <Meta property="twitter:card" content="summary_large_image" />
+    <Meta property="twitter:url" :content="loc.origin + loc.pathname" />
+    <Meta property="twitter:title" :content="data?.postBy?.title" />
+    <Meta property="twitter:description" :content="data?.postBy?.excerpt" />
+    <Meta property="twitter:image" :content="data?.postBy?.featuredImage.node.mediaItemUrl" />
   </Head>
   <div>
     <img v-if="data?.postBy?.featuredImage" :src="data?.postBy?.featuredImage.node.mediaItemUrl" class="rounded mb-4"
@@ -36,14 +46,17 @@ query getPostBySlug($slug: String) {
 `
 const route = useRoute()
 const variables = { slug: route.params.slug }
-const { data } = await useAsyncQuery(query, variables)
+const { data } = await useAsyncQuery(query, variables);
+const loc = window.location
 
+console.log('route:', route)
+console.log('loc:', window.location)
 
-useServerSeoMeta({
-  title: () => `${data.postBy?.title} - Le Bus Magique`,
-  ogTitle: () => `${data.postBy?.title} - Le Bus Magique`,
-  description: () => data.postBy?.excerpt,
-  ogDescription: () => data.postBy?.excerpt,
-})
+// useServerSeoMeta({
+//   title: () => `${data.postBy?.title} - Le Bus Magique`,
+//   ogTitle: () => `${data.postBy?.title} - Le Bus Magique`,
+//   description: () => data.postBy?.excerpt,
+//   ogDescription: () => data.postBy?.excerpt,
+// })
 
 </script>
